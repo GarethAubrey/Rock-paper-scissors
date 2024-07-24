8f1ba3abb362859dc7ee336eb5487ef201050ce2
Gareth Aubrey <aubreygareth@gmail.com>
2024-07-24 10:26:42 +0000

    Add let result with if/else

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,32 @@
 // Rock Paper scissors spock
 
-const option = ["rock", "paper", "scissors", "spock"];
+const choices = ["rock", "paper", "scissors",];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 
-function playGame(playerOption){
+function playGame(playerChoice) {
 
-    const 
+    const computerChoice = choices[Math.floor(Math.random() * 4)];
+    let result = "";
+
+    if(playerChoice === computerChoice){
+        result = "It's a draw!";
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+            result = (computerChoice === "scissors") ? "You win!" : "You lose!";
+            break;
+            case "paper":
+            result = (computerChoice === "rock") ? "You win!" : "You lose!";
+            break;
+            case "scissors":
+            result = (computerChoice === "paper") ? "You win!" : "You lose!";
+            break;
+        }
+    }
+
+    
+    
 }
