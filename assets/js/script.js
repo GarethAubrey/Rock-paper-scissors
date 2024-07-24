a548debe25e862a666a01405023a73802be5969e
Gareth Aubrey <aubreygareth@gmail.com>
2024-07-24 09:18:12 +0000

    Add start of html, css and javascript

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Rock Paper scissors spock
+
+const option = ["rock", "paper", "scissors", "spock"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerOption){
+
+    const 
+}
